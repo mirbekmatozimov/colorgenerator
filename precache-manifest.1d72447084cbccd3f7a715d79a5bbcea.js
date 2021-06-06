28770dc21db18ffa72af3a348d27174e4592b79a
mirbek <mirbek.25.52@gmail.com>
2021-06-06 18:25:35 +0600

    Updates

---
+++ precache-manifest.1d72447084cbccd3f7a715d79a5bbcea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5098d92e2f857bf897f6835faa22fb81",
+    "url": "/colorgenerator/index.html"
+  },
+  {
+    "revision": "e8cb8ba19eaf2667dcf4",
+    "url": "/colorgenerator/static/css/main.8b672847.chunk.css"
+  },
+  {
+    "revision": "cf6bca8e816ecf72b2d4",
+    "url": "/colorgenerator/static/js/2.11f691c1.chunk.js"
+  },
+  {
+    "revision": "1dbab337b90e8f2eb0a4a79d2378f49b",
+    "url": "/colorgenerator/static/js/2.11f691c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cb8ba19eaf2667dcf4",
+    "url": "/colorgenerator/static/js/main.7ca0cbef.chunk.js"
+  },
+  {
+    "revision": "9a81fc20ede65e217e84",
+    "url": "/colorgenerator/static/js/runtime-main.bd39945e.js"
+  }
+]);
